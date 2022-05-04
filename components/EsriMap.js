3775147b8a07fcfa2447cc3eba6e55a6dd9b2554
Max <maxjgruber@gmail.com>
2022-05-04 16:53:09 +0200

    Refactor arcgis map

--- components/EsriMap.js
+++ components/EsriMap.js
@@ -1,12 +1,73 @@
 import { loadModules } from "esri-loader";
+import useSWR from "swr";
+
+const LEGEND = [
+  {
+    color: "#36869a",
+    threshold: 60,
+    label: "> 60",
+  },
+  {
+    color: "#4facc5",
+    threshold: 50,
+    label: "50 - 60",
+  },
+  {
+    color: "#94cddb",
+    threshold: 40,
+    label: "40 - 50",
+  },
+  {
+    color: "#b7dde7",
+    threshold: 30,
+    label: "30 - 40",
+  },
+  {
+    color: "#dbeef6",
+    threshold: 0,
+    label: "< 30",
+  },
+  {
+    color: "#d8d8d8",
+    fallback: true,
+    label: "Información no disponible",
+  },
+];
+
+const getCountryFill = (score) => {
+  if (score == null) return LEGEND.find((item) => item.fallback).color;
+
+  return LEGEND.find((item) => score >= item.threshold).color;
+};
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const EsriMap = () => {
+  const { data, error } = useSWR(
+    "https://services7.arcgis.com/IyvyFk20mB7Wpc95/arcgis/rest/services/SDR_2021_Dataset/FeatureServer/0/query?where=1%3D1&outFields=id,sdgi_s&outSR=4326&f=json",
+    fetcher
+  );
+
+  console.log(data);
+
+  if (!data) return null;
+
+  const dataWithColor = data.features.map((feature) => ({
+    value: feature.attributes.id,
+    symbol: {
+      type: "simple-fill",
+      color: getCountryFill(feature.attributes.sdgi_s),
+    },
+  }));
+
+  console.log(dataWithColor);
+
   loadModules([
     "esri/config",
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/FeatureLayer",
-  ]).then(([esriConfig, Map, MapView, FeatureLayer]) => {
+    "esri/layers/GeoJSONLayer",
+  ]).then(([esriConfig, Map, MapView, GeoJSONLayer]) => {
     esriConfig.apiKey =
       "AAPKc0cb25ac2df940c0ade85be55fe1e373-99W-W4hV6xFLTYdnG8yrSgJeaF9lBD7cBH7G0oEzUrLumbmdiSItDxPHLx755w7";
 
@@ -21,21 +82,35 @@ const EsriMap = () => {
       container: "viewDiv", // Div element
     });
 
-    const popup = {
-      title: "POPUP",
-      content: "<h1>THIS IS A POPUP</h1>",
+    const colorRenderer = {
+      type: "unique-value",
+      defaultSymbol: {
+        color: "green",
+        type: "simple-fill",
+        style: "solid",
+        outline: {
+          color: "white",
+          width: 1,
+        },
+      },
+      field: "iso_a3",
+      uniqueValueInfos: dataWithColor,
     };
 
-    const layer = new FeatureLayer({
-      url: "https://services7.arcgis.com/IyvyFk20mB7Wpc95/arcgis/rest/services/ESDR2021Database/FeatureServer/0",
+    // const popup = {
+    //   title: "POPUP",
+    //   content: "<h1>THIS IS A POPUP</h1>",
+    // };
+
+    // create new geojson layer using the blob url
+    const layer = new GeoJSONLayer({
+      url: "/static/world.json",
+      outFields: ["*"],
+      renderer: colorRenderer,
+      opacity: 0.7,
     });
 
-    const geojson = new FeatureLayer({
-      url: "https://services7.arcgis.com/IyvyFk20mB7Wpc95/arcgis/rest/services/worldmap_geojson/FeatureServer/0",
-      popupTemplate: popup,
-    });
     map.add(layer);
-    map.add(geojson);
   });
 
   return (
